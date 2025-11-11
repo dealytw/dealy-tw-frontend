bb9d40e781e51faabd2e4fe51e0b4598b87950ae
dealytw <dealytw@gmail.com>
2025-11-11 22:11:41 +0000

    Add Google Tag Manager (GTM-N8WGJVQS) and fix siteUrl typo

--- app/layout.tsx
+++ app/layout.tsx
@@ -129,6 +129,20 @@ export default async function RootLayout({ children }: { children: React.ReactNo
   return (
     <html lang={htmlLang} suppressHydrationWarning>
       <head>
+        {/* Google Tag Manager */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-N8WGJVQS');
+            `,
+          }}
+        />
+        {/* End Google Tag Manager */}
+        
         {/* Preconnect to CMS for faster API calls */}
         <link rel="preconnect" href="https://cms.dealy.tw" crossOrigin="" />
         <link rel="dns-prefetch" href="//cms.dealy.tw" />
@@ -157,6 +171,17 @@ export default async function RootLayout({ children }: { children: React.ReactNo
         )}
       </head>
       <body suppressHydrationWarning>
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-N8WGJVQS"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          />
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
+        
         <Providers>
           <SearchProvider initialMerchants={searchMerchants}>
             {/* Site-wide JSON-LD: WebSite + Organization */}
