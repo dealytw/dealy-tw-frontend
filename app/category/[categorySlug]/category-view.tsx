e97ac6d05a2645177c2e3e3c254e0fe3ed47fe73
dealytw <dealytw@gmail.com>
2025-11-13 23:16:12 +0000

    Update merchant cards grid to fill available space better

--- app/category/[categorySlug]/category-view.tsx
+++ app/category/[categorySlug]/category-view.tsx
@@ -73,7 +73,7 @@ export default function CategoryView({
               本分類熱門商戶
             </h2>
             
-            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-7 gap-8">
+            <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 2xl:grid-cols-7 gap-6">
               {merchants.map((merchant) => (
                 <Link
                   key={merchant.id}
