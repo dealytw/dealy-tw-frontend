887261ca0e9114acbfb77dd9e238e123e900efbf
dealytw <dealytw@gmail.com>
2025-12-16 11:15:35 +0000

    Make sidebar scrollable while maintaining sticky behavior - Add max-h-[calc(100vh-6rem)] to limit sidebar height - Add overflow-y-auto for internal scrolling when content exceeds viewport - Sidebar now scrolls internally while remaining sticky at top - Follows page scroll naturally and stops sticking at container bottom

--- app/blog/[page_slug]/blog-view.tsx
+++ app/blog/[page_slug]/blog-view.tsx
@@ -593,8 +593,8 @@ export default function BlogView({ blog }: BlogViewProps) {
             )}
           </div>
 
-          {/* Sidebar - Sticky on desktop */}
-          <div className="lg:col-span-1 lg:sticky lg:top-24 lg:self-start lg:h-fit">
+          {/* Sidebar - Sticky on desktop with scrollable content */}
+          <div className="lg:col-span-1 lg:sticky lg:top-24 lg:self-start lg:h-fit lg:max-h-[calc(100vh-6rem)] lg:overflow-y-auto">
             <div className="space-y-6">
               {/* Related Articles */}
               <Card>
