5d10578328360ae9a2def441789ecc370ce7c95b
dealytw <dealytw@gmail.com>
2026-02-05 22:46:16 +0000

    Fix topicpage-sitemap: avoid nested template literal for Turbopack

--- app/topicpage-sitemap.xml/route.ts
+++ app/topicpage-sitemap.xml/route.ts
@@ -12,12 +12,14 @@ export async function GET() {
 
   const { pages } = await getTopicpageSitemapData(baseUrl, marketKey)
 
+  const urlEntries = pages.map(
+    (page) =>
+      '  <url>\n    <loc>' + page.url + '</loc>\n    <lastmod>' + page.lastmod + '</lastmod>\n  </url>'
+  ).join('\n')
+
   const xml = `<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-${pages.map(page => `  <url>
-    <loc>${page.url}</loc>
-    <lastmod>${page.lastmod}</lastmod>
-  </url>`).join('\n')}
+${urlEntries}
 </urlset>`
 
   return new NextResponse(xml, {
